d5fa46cf0a4fa4819d78306fdb84c6146cc9f04c
zhouxiangbo <453086912@qq.com>
2025-01-03 15:37:58 +0800

    dd

---
+++ precache-manifest.f9bb4f4b1f703a2ce191b1c8a44e0dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3d65435d356e88d25e7",
+    "url": "/react-three-obj-loader/static/css/main.77309cbc.chunk.css"
+  },
+  {
+    "revision": "b3d65435d356e88d25e7",
+    "url": "/react-three-obj-loader/static/js/main.ffb5501b.chunk.js"
+  },
+  {
+    "revision": "0e8e38612a717e79c2c4",
+    "url": "/react-three-obj-loader/static/js/runtime~main.1a6638de.js"
+  },
+  {
+    "revision": "6d3f0f9ff956ecc747c2",
+    "url": "/react-three-obj-loader/static/js/2.324a2c1a.chunk.js"
+  },
+  {
+    "revision": "798307d32a27fef5b653282c418fcf12",
+    "url": "/react-three-obj-loader/index.html"
+  }
+];
